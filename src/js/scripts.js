b3782133ed863173acd95043eb454a1618be5d03
Michaela Davis <michaela.delight@gmail.com>
2017-10-05 12:41:05 -0700

    🔩  Meta

---
+++ src/js/scripts.js
@@ -0,0 +1,84 @@
+(function($, HelloWorldDevsTysonSteele) {
+
+  // ======= Fixes =======
+  HelloWorldDevsTysonSteele.noOrphans('h1,h2,h3,h4,h5,h6,li,p', '.allow-orphan');
+  HelloWorldDevsTysonSteele.scrollToFix('#primary-menu');
+
+  // ======= Request Appointment Form =======
+  HelloWorldDevsTysonSteele.requestAppointment('#mail-form', '#success_msg' , '7fb35345-752d-4792-9480-cd3db6674a62');
+
+  // ======= Carousels =======
+  HelloWorldDevsTysonSteele.marqueeCarousel({
+    autoplay: 6000,
+    effect: 'fade',
+    speed: 500
+  });
+  HelloWorldDevsTysonSteele.tourCarousel('.js-tour-carousel', {
+    autoplay: true,
+    autoplayTimeout: 10000,
+    items: 1,
+    margin: 40,
+    navText: [
+      '<img src="assets/tour-left.png">',
+      '<img src="assets/tour-right.png">'
+    ],
+    autoHeight: true
+  });
+
+  HelloWorldDevsTysonSteele.associationCarousel('.js-associations-carousel', {
+    items: 2,
+    loop: true,
+    autoplay: true,
+    autoplaySpeed: 500,
+    autoplayTimeout: 2000,
+    autoplayHoverPause:false,
+    autoWidth: false,
+    nav: false,
+    dots: false,
+    margin: 40,
+    slideBy: 1,
+    responsive: {
+      550: {
+        items: 3,
+        autoplay: false
+      },
+      992: {
+        items: 3,
+        autoplay: false
+      }
+    }
+  });
+  HelloWorldDevsTysonSteele.pyramidCarousel('.js-team-carousel', {
+    items: 1,
+    autoplay: true,
+    navText: [
+      '<img src="assets/arrow-left.png">',
+      '<img src="assets/right-arrow.png">'
+    ],
+    responsive: {
+      550: {
+        items: 2
+      },
+      768: {
+        items: 3
+      },
+      992: {
+        items: 4,
+        nav: false,
+        autoplay: false
+      }
+    },
+    margin: 15
+  });
+  HelloWorldDevsTysonSteele.updateCopyright('.js-copyright-year');
+
+  // ======= Google Maps =======
+  HelloWorldDevsTysonSteele.googleMap('#google-map5', '47.694916,-114.1635727', '47.694916,-114.1635727');
+
+  $('#gallery-btn').click( function(e) {
+    e.preventDefault();
+    $('#gallery-target').click();
+  });
+
+}(jQuery, HelloWorldDevsTysonSteele));
+
